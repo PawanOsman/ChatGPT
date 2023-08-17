fadbc1f1b14144f0b8b3eacf90c97fcd3efcd72e
ZYX <zyxemail.24@gmail.com>
2023-08-17 16:36:37 +0700

    I dont care

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ export const SERVER_PORT = 3000; // Server port
 export const DEBUG = false; // Debug mode
 
 // Prompt Moderation before sending to OpenAI
-export const MODERATION = true; // Moderation mode
+export const MODERATION = false; // Moderation mode
 
 // Rate limit
 export const PRIOD = 15 * 1000; // 15 seconds
@@ -11,13 +11,13 @@ export const RATE_LIMIT = 50; // 50 requests per 15 seconds
 
 // Whitelisted IPs
 export const WHITELISTED_IPS = [
-    // "127.0.0.1"
+     "127.0.0.1"
 ];
 
 // OpenAI API Keys
 export let OPENAI_KEYS = [
-    "sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    "sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    "sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    "sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    "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",
+    "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",
+    "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",
+    "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",
 ];
